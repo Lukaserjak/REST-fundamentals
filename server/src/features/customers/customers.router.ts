0ecc91fd2f74f261e369606244db53913cf2f0f7
Lukaserjak <serjak.luka@gmail.com>
2025-11-03 13:45:38 +0000

    some changes

--- server/src/features/customers/customers.router.ts
+++ server/src/features/customers/customers.router.ts
@@ -1,3 +1,60 @@
 import express from "express";
+import { getCustomerDetail, getCustomers, searchCustomers, upsertCustomer } from "./customers.service";
+import { customerPOSTRequestSchema, idItemIdUUIDRequestSchema, idUUIDRequestSchema } from "../types";
+import { getOrders, getOrdersForCustomer } from "../orders/orders.service";
+import { Validator } from "express-oauth2-jwt-bearer";
+import { validate } from "../../middleware/validation.middleware";
+import { create } from "domain";
 
 export const customersRouter = express.Router();
+
+
+customersRouter.get("/:id", async(req, res) =>
+{
+    const id = req.params.id;
+    const customer = await getCustomerDetail(id);
+    if (customer != null)
+    {
+        res.json(customer);
+    }
+    else
+    {
+        res.status(404).json({message:"Customer not found"})
+    }
+});
+
+customersRouter.get("/:id/orders", async(req, res) =>
+{
+    const id = req.params.id;
+    const orders = await getOrdersForCustomer(id);
+    res.json(orders);
+});
+
+customersRouter.get("/search/:query", async(req, res) =>
+{
+    const query = req.params.query;
+    const customer = await searchCustomers(query);
+    res.json(customer);
+});
+
+
+customersRouter.post("/", validate(customerPOSTRequestSchema), async(req, res) =>
+{
+    const data = customerPOSTRequestSchema.parse(req);
+    const customer = await upsertCustomer(data.body);
+    if(customer != null)
+    {
+        if (req.header["accept"] == "application/xml")
+        {
+            res.status(201).send(create().ele("customer", customer).end());
+        }
+        else
+        {
+            res.status(201).json(customer);
+        }
+    }
+    else
+    {
+        res.status(500).json({message: "Creation failed."});
+    }
+});
--- server/src/features/orders/orders.router.ts
+++ server/src/features/orders/orders.router.ts
@@ -1,3 +1,82 @@
 import express from "express";
+import { addOrderItems, deleteOrderItem, getOrderDetail, getOrders, upsertOrder } from "./orders.service";
+import { validate } from "../../middleware/validation.middleware";
+import { idItemIdUUIDRequestSchema, idUUIDRequestSchema, orderItemsDTORequestSchema, orderPUTRequestSchema, pagingRequestSchema } from "../types";
 
 export const ordersRouter = express.Router();
+
+
+
+ordersRouter.get("/", validate(pagingRequestSchema), async(req, res) =>
+{
+    const data = pagingRequestSchema.parse(req);
+    const orders = await getOrders(data.query.skip, data.query.take); 
+    res.json(orders);
+});
+
+
+ordersRouter.get("/:id", validate(idUUIDRequestSchema), async(req, res) =>
+{
+    const data = idUUIDRequestSchema.parse(req);
+    const order = await getOrderDetail(data.params.id);
+    if (order != null)
+    {
+        res.json(order);
+    }
+    else
+    {
+        res.status(400).json({message: "Order not found." });
+    }
+});
+
+ordersRouter.post("/:id/items", validate(orderItemsDTORequestSchema), async(req, res) =>
+{
+    const data = orderItemsDTORequestSchema.parse(req);
+    const order = await addOrderItems(data.params.id, data.body);
+    
+    if(data != null)
+    {
+        res.status(201).json(order);
+    }
+    else
+    {
+        res.status(500).json({message: "Addition failed"});
+    }
+
+
+}
+);
+
+
+
+ordersRouter.delete("/:id/items/:itemId", validate(idItemIdUUIDRequestSchema), async(req, res) =>
+{
+    const data = idItemIdUUIDRequestSchema.parse(req);
+    const order = await deleteOrderItem(data.params.id, data.params.itemId);
+    if (order != null)
+    {
+        res.json(order);
+    }
+    else
+    {
+        res.status(404).json({message: "Order or item not found."});
+    }
+});
+
+
+ordersRouter.put("/:id", validate(orderPUTRequestSchema), async(req, res) =>
+{
+    const data = orderPUTRequestSchema.parse(req);
+    const orderData = {customerId: "", ...data.body};
+    const order = await upsertOrder(orderData, data.params.id);
+    if (order != null)
+    {
+        res.json(order);
+    }
+    else
+    {
+        res.status(404).json({message: "Order or item not found."});
+    }
+})
+
+
